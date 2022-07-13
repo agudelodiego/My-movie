108d3f3255c2023d742e934d81f63f06d37f135e
Diego Agudelo <agudelodiego22@gmail.com>
2022-07-13 08:52:41 -0500

    Diseñe el componente card src/componentes/Cards.js  Este componente es el que va a contener la imagen, el titulo y el id de la pelicula mediante props, aun falta por terminar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import NavBar from "./componentes/NavBar";
+import Card from "./componentes/Cards.js";
 import {GetPopulars,GetPoster,GetDetails} from "./Model/Model.js"
 
 
@@ -8,6 +9,9 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="Pruebas">
+        <Card id="iD-ejemplo" path="https://rtvc-assets-radionica3.s3.amazonaws.com/s3fs-public/field/image/article/final-rick-morty-radionica.jpg" descripcion="imgen de prueba" titulo="Rick con su cerebelo hueco" />
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Cards.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/Card.css';
+
+
+
+// El componente card es el que llevara en su interior a cada una de las peliculas que devuelva la API 
+const Card = (props) =>{
+
+    return(
+        <figure className="Card" id={props.id}>
+            <div className="Card__posterContainer">
+                <img src={props.path} alt={props.descripcion} className="Card__poster" />    
+            </div>
+            <figcaption className="Card__tituloContainer">
+                <h2 className="Card__titulo">{props.titulo}</h2>
+            </figcaption>
+        </figure>
+    );
+}
+
+
+export default Card;
