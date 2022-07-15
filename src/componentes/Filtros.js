439d32310f4432a7a489d50c6140e7030c50a04b
Diego Agudelo <agudelodiego22@gmail.com>
2022-07-15 10:53:05 -0500

    Le hice algunos ajustes a la barra de navegacion y a los filtros

--- src/componentes/Filtros.js
+++ src/componentes/Filtros.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../styles/Filtros.css'
+import React, {useState} from "react";
+import "../styles/Filtros.css";
 
 
 
@@ -7,27 +7,28 @@ import '../styles/Filtros.css'
 
 const Filtros = () => {
 
+    // Utilizamos un hook de estado
+    let [filtro, setFiltro] = useState("Peliculas populares");
+
     return (
         <form className="desplegable">
-            <div className="desplegable__selectContainer" onClick={desplegar} >
-                <div className="desplegable__select">
-                    <img className="desplegable__icon" src= {require('../icons/favorito.png')} alt="Icono de favorito" />
-                    <p>Peliculas populares</p>
-                </div>
-                <img className="desplegable__icon-flecha desplegable__icon" src={require('../icons/flecha-desplegable.png')} alt="Icono de flecha" />
+            
+            <div className="desplegable__select" onClick={desplegar}>
+                <img className="desplegable__icon" src= {require("../icons/flecha-azul.png")} alt="Icono de favorito" />
+                <h2>{filtro}</h2>
             </div>
 
             <div className="desplegable__opciones">
-                <div className="desplegable__opcion" id="populares" >
-                    <img className="desplegable__icon" src= {require('../icons/favorito.png')} alt="Icono de favorito" />
+                <div className="desplegable__opcion" id="populares" onClick={()=>setFiltro("Peliculas populares")} >
+                    <img className="desplegable__icon" src= {require("../icons/favorito.png")} alt="Icono de favorito" />
                     <p>Peliculas populares</p>
                 </div>
-                <div className="desplegable__opcion" id="proximamente" >
-                    <img className="desplegable__icon" src={require('../icons/esperar.png')} alt="Icono de reloj de arena" />
+                <div className="desplegable__opcion" id="proximamente" onClick={()=> setFiltro("Nuevos lanzamientos")} >
+                    <img className="desplegable__icon" src={require("../icons/esperar.png")} alt="Icono de reloj de arena" />
                     <p>Nuevos lanzamientos</p>
                 </div>
-                <div className="desplegable__opcion" id="cartelera" >
-                    <img className="desplegable__icon" src={require('../icons/cartelera.png')} alt="Icono de cartelera" />
+                <div className="desplegable__opcion" id="cartelera" onClick={() => setFiltro("Peliculas en cartelera")} >
+                    <img className="desplegable__icon" src={require("../icons/cartelera.png")} alt="Icono de cartelera" />
                     <p>Peliculas en cartelera</p>
                 </div>
             </div>
@@ -41,7 +42,6 @@ const desplegar = () => {
     let opciones = document.querySelector(".desplegable__opciones");
     opciones.classList.toggle("show");
 }
-
 
 
 
